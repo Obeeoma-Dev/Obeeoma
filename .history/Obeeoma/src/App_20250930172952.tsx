cb59592ea69bfb80ea13b370318d2c8e6c9a5101
martha nakkintu <marthanakintu95@gmail.com>
2025-09-30 21:19:13 +0300

    new pages added

---
+++ .history/Obeeoma/src/App_20250930172952.tsx
@@ -0,0 +1,27 @@
+// App.tsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import CreateAccount from "./pages/CreateAccount";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import ResetPasswordSignIn from "./auth/ResetPasswordSignin";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Default route -> Login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Auth routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<CreateAccount />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/reset-password-signin" element={<ResetPasswordSign />} />
+
+
+        {/* Fallback */}
+        <Route path="*" element={<h1 className="text-center mt-20 text-xl">404 - Page Not Found</h1>} />
+      </Routes>
+    </Router>
+  );
+}
