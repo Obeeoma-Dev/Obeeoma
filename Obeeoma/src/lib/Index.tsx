b0d65219150a2e64d851ca76e55d396f4350acc8
Conrad Waako K <68024054+elconrad23@users.noreply.github.com>
2025-09-30 16:16:41 +0300

    Create Index.tsx

---
+++ Obeeoma/src/lib/Index.tsx
@@ -0,0 +1,21 @@
+mport Navigation from "@/components/Navigation";
+import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
+import Services from "@/components/Services";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <HowItWorks />
+      <Services />
+      <Features />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
