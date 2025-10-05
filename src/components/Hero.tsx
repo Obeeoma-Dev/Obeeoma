1b8a672e5441b39c0b8e7eb6a61a064fa9349af0
Conrad Waako K <eleazarconrad@gmail.com>
2025-10-06 00:48:12 +0300

    headerimage errorr

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import heroImage from "@/assets/images/headerimage.png";
+import heroImage from "@/assets/Images/headerimage.png";
 import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
