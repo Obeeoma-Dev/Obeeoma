0162d0c1cf9320db5d0f5e206d9e1c3f0cecf85a
Conrad Waako K <eleazarconrad@gmail.com>
2025-10-09 23:11:24 +0300

    buttonvariant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,29 +1,28 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import { type VariantProps } from "class-variance-authority";
-import { buttonVariants } from "./button.styles";
-
 import { cn } from "@/lib/utils";
+import { buttonVariants, ButtonVariantProps } from "./button.styles";
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
-  const Comp = asChild ? Slot : "button";
-
-  return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
-  );
+interface ButtonProps
+  extends React.ComponentProps<"button">,
+    ButtonVariantProps {
+  asChild?: boolean;
 }
 
-export { Button, buttonVariants };
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button };
