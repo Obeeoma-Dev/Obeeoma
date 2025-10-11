a30467952caaf79efa63091c1546a99a1a2c3ec1
Orena Gideon <orenagedion2020@gmail.com>
2025-10-11 11:03:52 +0300

    Doing a prettier write to pass the job step

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -37,7 +37,7 @@ function ToggleGroup({
       data-size={size}
       className={cn(
         "group/toggle-group flex w-fit items-center rounded-md data-[variant=outline]:shadow-xs",
-        className
+        className,
       )}
       {...props}
     >
@@ -73,7 +73,7 @@ function ToggleGroupItem({
         }),
         // Additional layout and focus styles
         "min-w-0 flex-1 shrink-0 rounded-none shadow-none first:rounded-l-md last:rounded-r-md focus:z-10 focus-visible:z-10 data-[variant=outline]:border-l-0 data-[variant=outline]:first:border-l",
-        className
+        className,
       )}
       {...props}
     >
