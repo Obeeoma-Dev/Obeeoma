3be2cfc1057bb520ac465e01c3543f56d562761b
Conrad Waako K <eleazarconrad@gmail.com>
2025-10-23 15:39:30 +0300

    modularized auth layout logo added

--- src/components/shared/AuthLayout.tsx
+++ src/components/shared/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/Images/obeeomalogoicon2.png';
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
