039745cd66fd57ca22d321d9319c8f5e5d3e962e
namugarurasyda <2100813912@std.kyu.ac.ug>
2025-11-17 13:36:12 -0800

    Adding a sucess page to the subscription component

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Formik, Form as FormikForm, ErrorMessage, Field as FormikField } from "formik";
