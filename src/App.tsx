bc8bdea3e9e63b4720651253a32d39f412579513
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-03-23 10:38:31 +0300

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { analytics } from "./services/analytics";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import ProtectedRoute from "./components/ProtectedRoute";
+// Commenting out the HistoryGuard
 // import HistoryGuard from "./components/HistoryGuard";
 
 // Pages
